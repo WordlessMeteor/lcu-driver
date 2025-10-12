eee11b2cb2d22b25513b7f80fac52d62c26bd8d2
WordlessMeteor <97432897+WordlessMeteor@users.noreply.github.com>
2025-10-12 13:37:13 +0800

    Multi-Client and No-Client Detection and Performance Optimization
    1. Adapted `utils._return_ux_process` function, so that a list of processes, instead of only one process, are returned.
    Discussion:
    By a generator returned, the memory used is supposed to be significantly reduced, but in this case, since a user normally won't run multiple League Clients on one device, the memory cost difference between returning a generator and returning a list of processes shouldn't be too much.
    2. Discuss about the number of running League Clients (namely the number of LeagueClientx.exe) through `connector.chooseClient` function.
    (1) If no client is found, raise a new error `NoLeagueClientDetected`;
    (2) if one client is found, build connection with that process;
    (3) otherwise, display the process list and let the user decide which client process to build connection with.
    While I was rewriting `utils._return_ux_process` function, two optimizations were made.
    1. Optimized the running time of `_return_ux_process` funtion.
    (1) Avoiding a zombie process by always checking a process' `status` method significantly increases the time expense. Therefore, A try-except statement is used to handle exceptions triggered by accessing the attribute of a zombie process.
    (2) (Windows only) Accessing the cmdline attribute of a process significantly increases the time expense. Since LeagueClientUx.exe always has the correct name on Windows, there's no need to access a process' cmdline attribute on Windows.
    2. Adjusted some type annotations. Note that these type annotations rely on Python ≥ 3.9.

--- lcu_driver/connector.py
+++ lcu_driver/connector.py
@@ -2,13 +2,52 @@ import asyncio
 import logging
 import time
 from abc import ABC, abstractmethod
+from psutil import Process # Type annotation only
 
 from .connection import Connection
 from .events.managers import ConnectorEventManager, WebsocketEventManager
 from .utils import _return_ux_process
+from .exceptions import NoLeagueClientDetected # Handles one case of the number of League Clients
 
 logger = logging.getLogger('lcu-driver')
 
+def chooseClient(processList: list[Process]): # Allows users to select one running League Client
+    if isinstance(processList, list) and all(map(lambda x: isinstance(x, Process), processList)):
+        if len(processList) == 0:
+            raise NoLeagueClientDetected()
+        elif len(processList) == 1:
+            process = processList[0]
+        else:
+            print('Multiple League Clients are detected. Please select one process to continue: (Submit "0" to exit.)')
+            # Optimize the prompt layout by formatting each column's width
+            indexWidth: int = max(map(lambda x: len(str(x + 1)), range(len(processList))))
+            pidWidth: int = max(map(lambda x: len(str(x.pid)), processList))
+            statusWidth: int = max(map(lambda x: len(str(x.status())), processList))
+            createTimeWidth: int = max(map(lambda x: len(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x.create_time())))), processList))
+            filePathWidth: int = max(map(lambda x: len(str(x.exe())), processList))
+            print("{0:^{5}}{1:^{6}}{2:^{7}}{3:^{8}}{4:^{9}}".format("No.", "pid", "status", "createTime", "filePath", indexWidth, pidWidth, statusWidth, createTimeWidth, filePathWidth))
+            for i in range(len(processList)):
+                process = processList[i]
+                procId: int = process.pid
+                procStatus: str = process.status() # Based on the status, this table may only display the running processes in a future commit
+                procCreateTime: str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(process.create_time()))
+                procFilePath: str = process.exe()
+                print("{0:^{5}}{1:^{6}}{2:^{7}}{3:^{8}}{4:^{9}}".format(i + 1, procId, procStatus, procCreateTime, procFilePath, indexWidth, pidWidth, statusWidth, createTimeWidth, filePathWidth))
+            while True:
+                processIndex = input()
+                if processIndex == "":
+                    continue
+                elif processIndex == "0":
+                    exit(0)
+                elif processIndex in set(map(str, range(1, len(processList) + 1))):
+                    processIndex = int(processIndex) - 1
+                    break
+                else:
+                    print("Please input an integer between 1 and %d." %(len(processList)))
+            process = processList[processIndex]
+        return process
+    else:
+        raise TypeError('invalid type of parameter "processList". Pass a list of Process objects instead')
 
 class BaseConnector(ConnectorEventManager, ABC):
     def __init__(self, loop=None):
@@ -61,11 +100,8 @@ class Connector(BaseConnector):
         """
         try:
             def wrapper():
-                process = next(_return_ux_process(), None)
-                while not process:
-                    process = next(_return_ux_process(), None)
-                    time.sleep(0.5)
-
+                processList: list[Process] = _return_ux_process()
+                process = chooseClient(processList)
                 connection = Connection(self, process)
                 self.register_connection(connection)
                 self.loop.run_until_complete(connection.init())
@@ -116,15 +152,11 @@ class MultipleClientConnector(BaseConnector):
         tasks = []
         try:
             while True:
-                process_iter = _return_ux_process()
-
-                process = next(process_iter, None)
-                while process:
+                processList: list[Process] = _return_ux_process()
+                for process in processList:
                     connection = Connection(self, process)
                     if not self._process_was_initialized(connection):
                         tasks.append(asyncio.create_task(connection.init()))
-
-                    process = next(process_iter, None)
                 await asyncio.sleep(0.5)
 
         except KeyboardInterrupt:
--- lcu_driver/utils.py
+++ lcu_driver/utils.py
@@ -1,10 +1,9 @@
-from typing import Dict, Generator, List
+import platform
+from psutil import ZombieProcess, AccessDenied, Process, process_iter
 
-from psutil import STATUS_ZOMBIE, Process, process_iter
 
-
-def parse_cmdline_args(cmdline_args) -> Dict[str, str]:
-    cmdline_args_parsed = {}
+def parse_cmdline_args(cmdline_args: list[str]) -> dict[str, str]:
+    cmdline_args_parsed: dict[str, str] = {}
     for cmdline_arg in cmdline_args:
         if len(cmdline_arg) > 0 and "=" in cmdline_arg:
             key, value = cmdline_arg[2:].split("=", 1)
@@ -12,17 +11,26 @@ def parse_cmdline_args(cmdline_args) -> Dict[str, str]:
     return cmdline_args_parsed
 
 
-def _return_ux_process() -> Generator[Process, None, None]:
-    for process in process_iter(attrs=["cmdline"]):
-        if process.status() == STATUS_ZOMBIE:
+def _return_ux_process() -> list[Process]:
+    processList: list[Process] = []
+    osPlatform: str = platform.system() # Distinguish the operating system preemptively
+    seen_pid: set[int] = set() # Ensure processList's uniqueness
+    for process in process_iter(): #attrs=["cmdline"] greatly increases time cost on Windows
+        try:
+            name: str = process.name()
+            if osPlatform in {"Linux", "Darwin"}:
+                cmdline: list[str] = process.cmdline()
+        except (ZombieProcess, AccessDenied): # Accessing the status method significantly increases time expense. This try-except statement should optimize this issue
             continue
+        else:
+            if name in {"LeagueClientUx.exe", "LeagueClientUx"}:
+                processList.append(process)
+                seen_pid.add(process.pid)
 
-        cmdline: List[str] = process.info.get("cmdline", [])
-
-        if process.name() in ["LeagueClientUx.exe", "LeagueClientUx"]:
-            yield process
-
-        # Check cmdline for the executable, especially useful in Linux environments
-        # where process names might differ due to compatibility layers like wine.
-        if cmdline and cmdline[0].endswith("LeagueClientUx.exe"):
-            yield process
+            if osPlatform in {"Linux", "Darwin"}: # In case the same process would be added multiple times on Windows
+                # Check cmdline for the executable, especially useful in Linux environments
+                # where process names might differ due to compatibility layers like wine.
+                if cmdline and cmdline[0].endswith("LeagueClientUx.exe") and not process.pid in seen_pid:
+                    processList.append(process)
+                    seen_pid.add(process.pid)
+    return processList
